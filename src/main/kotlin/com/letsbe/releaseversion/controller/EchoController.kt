d6b633d64ff193866d995ca3c343c496819a98a3
Letsbe-x <10years.shimhyeonggwan@gmail.com>
2023-11-01 01:14:04 +0900

    fix: echoController RequestParams 추가

--- src/main/kotlin/com/letsbe/releaseversion/controller/EchoController.kt
+++ src/main/kotlin/com/letsbe/releaseversion/controller/EchoController.kt
@@ -1,13 +1,14 @@
 package com.letsbe.releaseversion.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EchoController {
 
     @GetMapping("/echo")
-    fun echo(): String {
-        return "echo"
+    fun echo(@RequestParam hello:String): String {
+        return "Letsbe $hello"
     }
 }
