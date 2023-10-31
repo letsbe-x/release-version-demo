9c1a34e04a5fc285f75efd6838a49649844c5881
Letsbe-x <10years.shimhyeonggwan@gmail.com>
2023-11-01 00:52:35 +0900

    fix(dependency): switch dependency 'spring-boot-starter-web'

---
+++ src/main/kotlin/com/letsbe/releaseversion/controller/EchoController.kt
@@ -0,0 +1,13 @@
+package com.letsbe.releaseversion.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EchoController {
+
+    @GetMapping("/echo")
+    fun echo(): String {
+        return "echo"
+    }
+}
