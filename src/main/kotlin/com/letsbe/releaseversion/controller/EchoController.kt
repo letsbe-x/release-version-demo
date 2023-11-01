e9f8dd64f48e03e46538ba126e17693c3d344a48
Letsbe-x <10years.shimhyeonggwan@gmail.com>
2023-11-01 10:51:03 +0900

    fix: revert EchoController

--- src/main/kotlin/com/letsbe/releaseversion/controller/EchoController.kt
+++ src/main/kotlin/com/letsbe/releaseversion/controller/EchoController.kt
@@ -1,14 +1,13 @@
 package com.letsbe.releaseversion.controller
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EchoController {
 
     @GetMapping("/echo")
-    fun echo(@RequestParam hello:String): String {
-        return "Letsbe $hello"
+    fun echo(): String {
+        return "Letsbe"
     }
 }
